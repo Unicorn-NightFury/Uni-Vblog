bca478753c9bf178b472c8fed2b844b5b7a96563
Uni <13670210824@163.com>
2020-05-03 22:29:08 +0800

    feat；路由测试

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,26 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params;
+  ctx.body = {
+    title:"this is profile page!",
+    userName
+  }
+})
+
+router.get('/loadmore/:userName/:indexPage', async (ctx, next) => {
+  const { userName, indexPage } = ctx.params;
+  ctx.body = {
+    title: "this is loadmore page!",
+    userName,
+    indexPage
   }
 })
 
--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,13 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', async (ctx, next) => {
+  const { username, password } = ctx.request.body;
+  ctx.body = {
+    title: "这个是登录页面",
+    username,
+    password
+  }
+})
+
 module.exports = router
