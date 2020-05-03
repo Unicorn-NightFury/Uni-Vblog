61ce2e7024272cdf733a81f958869ae64714f3ae
Uni <13670210824@163.com>
2020-05-03 14:29:56 +0800

    初始化 Koa2 开发环境

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const index = require('../routes/index')
+const users = require('../routes/users')
 
 // error handler
 onerror(app)
