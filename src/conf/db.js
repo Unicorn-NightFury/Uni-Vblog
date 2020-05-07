abe16c0068b66c3cb090553568b94113fb58de20
Uni <13670210824@163.com>
2020-05-07 21:14:42 +0800

    feat: 增加 MySQL 以及 sequelize 配置

--- src/conf/db.js
+++ src/conf/db.js
@@ -4,10 +4,18 @@
  */
 
 const  { isProd } = require('../utils/env');
-
+                                            
 let REDIS_CONF = {
-port: 6379,
-host:'127.0.0.1'
+    port: 6379,
+    host:'127.0.0.1'
+}
+
+let MYSQL_CONF = {
+    host:'127.0.0.1',
+    user: 'root',
+    password: 'leo0824lch',
+    port: '3306',
+    database: 'vblog'
 }
 
 if (isProd) {
@@ -15,8 +23,17 @@ if (isProd) {
         port: 6379,
         host: '127.0.0.1'    // 先占位，后面再配置上线 ip
     }
+
+    MYSQL_CONF = {
+        host:'127.0.0.1',
+        user: 'root',
+        password: 'leo0824lch',
+        port: '3306',
+        database: 'vblog'
+    }
 }
 
 module.exports = {
-REDIS_CONF
+    REDIS_CONF,
+    MYSQL_CONF
 }
--- src/utils/env.js
+++ src/utils/env.js
@@ -5,9 +5,11 @@
 
  const ENV = process.env.NODE_ENV;
 
- module.exports = {
-    isDev: ENV === 'dev',
-    notDev: ENV !== 'dev',
-    isProd: ENV === 'production',
-    notProd: ENV !== 'production'
- }
+module.exports = {
+   isDev: ENV === 'dev',
+   notDev: ENV !== 'dev',
+   isProd: ENV === 'production',
+   notProd: ENV !== 'production',
+   isTest: ENV === 'test',
+   notTest: ENV !== 'test',
+}
